9f192df858dde28d97b7bfd6ad669107eb78a810
Darko Stankovski <darko.itpro@gmaiL.com>
2025-02-10 14:52:44 +0100

    Demi Skip

--- tests/home/test_skipped_heath_status.py
+++ tests/home/test_skipped_heath_status.py
@@ -2,8 +2,8 @@ import pytest
 import sys
 from project.home.status import heat_status
 
-skipsystem = pytest.mark.skipif(sys.platform == "linux", reason="not implemented")
-
+if sys.version_info >= (3, 8):
+    pytest.skip("Demo for all", allow_module_level=True)
 
 def test_status_hot():
     assert heat_status(25) == "Chaud", "Un truc qui va pas"
@@ -11,14 +11,11 @@ def test_status_hot():
 def test_status_cold():
     assert heat_status(10) == "Froid"
 
-@skipsystem
 def test_status_good():
     assert heat_status(20) == "Bon"
 
-@pytest.mark.skip(reason="Attente ticke #4564")
 def test_limit_cold():
     assert heat_status(18) == "Bon"
 
-@pytest.mark.skip(reason="Attente ticke #4564bis")
 def test_limit_hot():
     assert heat_status(22) == "Bon"
