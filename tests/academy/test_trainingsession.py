18a42d1bad8bb8813a5f7286a8993c3e7ba4d24c
Darko Stankovski <darko.itpro@gmaiL.com>
2025-02-11 11:21:25 +0100

    Tests Ajout méthode paramétrée

--- tests/academy/test_trainingsession.py
+++ tests/academy/test_trainingsession.py
@@ -9,6 +9,13 @@ class TestTrainingSessionInitiation:
         assert session.seats_left == 10
         assert len(session.students) == 0
 
+    duration_exception = [0, -1]
+
+    @pytest.mark.parametrize("duration", duration_exception)
+    def test_must_have_one_day(self, duration):
+        with pytest.raises(ValueError):
+            TrainingSession("formation python", duration, 10)
+
 class TestAddStudent:
     def test_add_first_student(self):
         session = TrainingSession("formation python", 4, 10)
